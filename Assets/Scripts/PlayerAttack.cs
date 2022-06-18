e5035fa5868cc2ab68e031fb55b1ed8e0e1abe85
Marcel Kolodziej <kolodziejmarcel1@gmail.com>
2022-06-18 02:13:06 +0200

    initial commit

---
+++ Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    [SerializeField] private float attackCooldown;
+    private AnimationScript anim;
+    private PlayerMovement playerMovement;
+    private float coolDownTimer = Mathf.Infinity;
+
+    
+    private void Awake()
+    {
+        anim = GetComponentInChildren<AnimationScript>();
+        playerMovement = GetComponent<PlayerMovement>();
+    }
+
+
+    private void Update()
+    {
+
+        if (Input.GetMouseButton(1) && coolDownTimer > attackCooldown && playerMovement.canAttack())
+        Attack();
+
+        coolDownTimer += Time.deltaTime;
+    }
+
+    private void Attack()
+    {
+        Debug.Log("attack Player Attack");
+        anim.SetTrigger("attack");
+        coolDownTimer = 0;
+    }
+
+}
